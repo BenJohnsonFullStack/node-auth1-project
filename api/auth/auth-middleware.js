e07704177cd8309dbf1706611c16ef8aeeb0895d
Ben Johnson <monk.sound.design@gmail.com>
2023-09-18 00:11:29 -0400

    middleware wired

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -6,7 +6,9 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {}
+function restricted(req, res, next) {
+  next();
+}
 
 /*
   If the username in req.body already exists in the database
@@ -16,7 +18,9 @@ function restricted() {}
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {}
+function checkUsernameFree(req, res, next) {
+  next();
+}
 
 /*
   If the username in req.body does NOT exist in the database
@@ -26,7 +30,9 @@ function checkUsernameFree() {}
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {}
+function checkUsernameExists(req, res, next) {
+  next();
+}
 
 /*
   If password is missing from req.body, or if it's 3 chars or shorter
@@ -36,7 +42,9 @@ function checkUsernameExists() {}
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {}
+function checkPasswordLength(req, res, next) {
+  next();
+}
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
 module.exports = {
