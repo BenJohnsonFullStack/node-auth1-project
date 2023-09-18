36db47c38e396d42a71b167da96c11b51bc752b4
Ben Johnson <monk.sound.design@gmail.com>
2023-09-18 00:28:44 -0400

    user-router endpoint

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -3,36 +3,35 @@ const db = require("../../data/db-config");
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
-function find() {
+function get() {
   return db("users");
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
-async function findBy(filter) {
-  const users = await db("users");
-  return users.filter(filter);
+function getBy(filter) {
+  return db("users").where(filter);
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
-function findById(id) {
-  return db("users").where("id", id).first();
+function getById(user_id) {
+  return db("users").where("id", user_id).first();
 }
 /**
   resolves to the newly inserted user { user_id, username }
  */
 async function add(user) {
-  await db("users").insert(user);
-  const newUser = findById(user.id);
+  const [user_id] = await db("users").insert(user);
+  const newUser = getById(user_id);
   return newUser;
 }
 // Don't forget to add these to the `exports` object so they can be required in other modules
 module.exports = {
-  find,
-  findBy,
-  findById,
+  get,
+  getBy,
+  getById,
   add,
 };
