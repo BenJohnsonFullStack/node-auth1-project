e7301b947f8179f8dc6a4b0249bfd27cfec86faa
Ben Johnson <monk.sound.design@gmail.com>
2023-09-18 00:32:00 -0400

    select columns in users GET

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -4,21 +4,21 @@ const db = require("../../data/db-config");
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function get() {
-  return db("users");
+  return db("users").select("user_id", "username");
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
 function getBy(filter) {
-  return db("users").where(filter);
+  return db("users").where(filter).select("user_id", "username");
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
 function getById(user_id) {
-  return db("users").where("id", user_id).first();
+  return db("users").where("id", user_id).first().select("user_id", "username");
 }
 /**
   resolves to the newly inserted user { user_id, username }
